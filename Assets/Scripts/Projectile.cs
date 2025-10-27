610300fd485536d97241f8a365eca6e3caa72ff8
Danilo Colella <danilocolella17@gmail.com>
2025-10-27 11:07:30 +0100

    add projectiles

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float moveSpeed;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.up * moveSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }
+
+        if (collision.gameObject.tag == "Boundary")
+        {
+            Destroy(gameObject);
+            
+        }
+    }
+}
