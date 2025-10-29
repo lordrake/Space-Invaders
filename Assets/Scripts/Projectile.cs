92149f693dcc18088982736f962cc343e06cb588
Danilo Colella <danilocolella17@gmail.com>
2025-10-29 06:40:06 +0100

    add victory screen

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -6,11 +6,17 @@ public class Projectile : MonoBehaviour
     public GameObject explostionPrefab;
     private PointManager pointManager;
     private int pointIncrement = 50;
+    private GameObject[] listOfEnemies;
+
+    public PlayerLives playerLives;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         pointManager = GameObject.Find("PointManager").GetComponent<PointManager>();
+        playerLives = GameObject.Find("PlayerShip").GetComponent<PlayerLives>();
+
         
     }
 
@@ -27,13 +33,26 @@ public class Projectile : MonoBehaviour
             Instantiate(explostionPrefab, transform.position, Quaternion.identity);
             Destroy(collision.gameObject);
             pointManager.UpdateScore(pointIncrement);
+            checkIfVictory();
             Destroy(gameObject);
         }
 
         if (collision.gameObject.tag == "Boundary")
         {
             Destroy(gameObject);
-            
+        }
+
+
+    }
+    
+    private void checkIfVictory()
+    {
+        listOfEnemies = GameObject.FindGameObjectsWithTag("Enemy");
+        Debug.Log(listOfEnemies.Length);
+
+        if (listOfEnemies.Length <= 1)
+        {
+            playerLives.ShowGameOver();
         }
     }
 }
